0f090993cf8ff2f3d86f599fa7a29daeab423b34
DaeHOHoHOHo <miraeho00@gmail.com>
2022-12-27 13:23:11 +0900

    13강 yarn install

--- 13_SPA/src/App.js
+++ 13_SPA/src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
-import { Routes } from "react-router-dom";
-import About from './About';
-import Home from "./Home";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  return(
-    <div>
-      <Routes path="/" component={Home} exact={true}/>
-      <Routes path="/about" component={About} />
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
