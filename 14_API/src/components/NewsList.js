3126e3688dda4d8e8e062aaad60fc6f013bda844
DaeHOHoHOHo <miraeho00@gmail.com>
2023-01-04 18:49:54 +0900

    category기능구현

--- 14_API/src/components/NewsList.js
+++ 14_API/src/components/NewsList.js
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
     };
 `;
 
-const NewsList = () => {
+const NewsList = ({category}) => {
     const [articles, setArticles] = useState(null);
     const [loading, setLoading] = useState(false);
 
@@ -24,8 +24,9 @@ const NewsList = () => {
         const fetchData = async() => {
             setLoading(true);
             try {
+                const query = category === 'all' ? '' : `&category=${category}`;
                 const response = await axios.get(
-                    'https://newsapi.org/v2/top-headlines?country=kr&category=entertainment&apiKey=76b29fc67ef245689255fcba9943b2a9',
+                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=76b29fc67ef245689255fcba9943b2a9`,
                 );
                 setArticles(response.data.articles);
             } catch(e) {
