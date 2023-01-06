aaa0ecdda5c9bc6c1bcd462988fb6fc8f32af681
DaeHOHoHOHo <miraeho00@gmail.com>
2023-01-06 18:18:45 +0900

    selectcolor done

--- 15_Context API/src/components/SelectColors.js
+++ 15_Context API/src/components/SelectColors.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ColorConsumer } from "../contexts/color";
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
@@ -6,7 +7,9 @@ const SelectColors = () => {
     return(
         <div>
             <h2>색상을 선택하세요.</h2>
-            <div style={{display: 'flex'}}>
+            <ColorConsumer>
+            {({actions}) => (
+                <div style={{display: 'flex'}}>
                 {colors.map(color => (
                     <div
                         key={color}
@@ -16,9 +19,16 @@ const SelectColors = () => {
                             height: '24px',
                             cursor: 'pointer'
                         }}
+                        onClick={() => actions.setColor(color)}
+                        onContextMenu={e => {
+                            e.preventDefault();
+                            actions.setSubColor(color);
+                        }}
                     />
                 ))}
             </div>
+            )}
+            </ColorConsumer>
             <hr />
         </div>
     );
